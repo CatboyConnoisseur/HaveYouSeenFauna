e511ce7630a7bbff5ba1d62f311b70513fbc11d6
CatboyConnoisseur <83247189+CatboyConnoisseur@users.noreply.github.com>
2024-10-24 16:17:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 // Array of image URLs
-const images = [
-    'images/fauna1.webp',
-    'images/fauna2.webp'
+const imagesWithLinks = [
+    { src: 'images/fauna1.webp', link: 'https://virtualyoutuber.fandom.com/wiki/Ceres_Fauna/Gallery' },
+    { src: 'images/fauna2.webp', link: 'https://virtualyoutuber.fandom.com/wiki/Ceres_Fauna/Gallery' },
+    { src: 'images/fauna3.webp', link: 'https://virtualyoutuber.fandom.com/wiki/Ceres_Fauna/Gallery' },
+    { src: 'images/fauna4.webp', link: 'https://virtualyoutuber.fandom.com/wiki/Ceres_Fauna/Gallery' },
+    { src: 'images/fauna5.webp', link: 'https://virtualyoutuber.fandom.com/wiki/Ceres_Fauna/Gallery' },
+    { src: 'images/fauna6.webp', link: 'https://virtualyoutuber.fandom.com/wiki/Ceres_Fauna/Gallery' }
 ];
 
-// Function to display a random image
+
+// { src: 'images/fauna.webp', link: 'link' }, 
+
+
 function displayRandomImage() {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    const randomImage = images[randomIndex];
-    
+    const randomIndex = Math.floor(Math.random() * imagesWithLinks.length);
+    const randomImage = imagesWithLinks[randomIndex];
+
     const imageElement = document.getElementById('randomImage');
-    imageElement.src = randomImage;
+    const imageLink = document.getElementById('imageLink');
+
+    // Set the image source
+    imageElement.src = randomImage.src;
+    
+    // Set the href of the link to match the image's link
+    imageLink.href = randomImage.link;
 }
 
 // Call the function when the page loads
